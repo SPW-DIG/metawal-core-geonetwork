04f9d56a55aa6c0bc79249ecd48c2f22e1ac1a4a
Arnaud De Groof <arnaud.degroof@spacebel.be>
2013-12-16 16:39:46 +0100

    WMC - comment background layers (#3)

--- web-client/src/main/resources/apps/search/js/map/Settings.js
+++ web-client/src/main/resources/apps/search/js/map/Settings.js
@@ -36,10 +36,10 @@ GeoNetwork.map.PROJECTION = "EPSG:4326";
 GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-180, -90, 180, 90);
 //GeoNetwork.map.EXTENT = new OpenLayers.Bounds(-5.1,41,9.7,51);
 
-GeoNetwork.map.BACKGROUND_LAYERS = [
-    new OpenLayers.Layer.WMS("Background layer", "/geoserver/wms", {layers: 'gn:world,gn:ne_50m_boundary_da,gn:ne_50m_boundary_lines_land,gn:ne_50m_coastline', format: 'image/jpeg'}, {isBaseLayer: true})
+//GeoNetwork.map.BACKGROUND_LAYERS = [
+    ///new OpenLayers.Layer.WMS("Background layer", "/geoserver/wms", {layers: 'gn:world,gn:ne_50m_boundary_da,gn:ne_50m_boundary_lines_land,gn:ne_50m_coastline', format: 'image/jpeg'}, {isBaseLayer: true})
     //new OpenLayers.Layer.WMS("Background layer", "http://www2.demis.nl/mapserver/wms.asp?", {layers: 'Countries', format: 'image/jpeg'}, {isBaseLayer: true})
-];
+//];
 
 //// Config for OSM based maps
 //GeoNetwork.map.PROJECTION = "EPSG:900913";
