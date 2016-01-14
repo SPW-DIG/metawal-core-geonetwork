8a58ba4a5c60d358ce1e24e57b7e87a44cd0700b
agf <arnaud.degroof@spacebel.be>
2016-01-14 15:06:01 +0100

    Improve social media links (#17)

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -39,6 +39,11 @@
             gnSearchManagerService, gnConfigService, gnConfig,
             gnGlobalSettings, $location, gnUtilityService, gnSessionService) {
       $scope.version = '0.0.1';
+      //Update Links for social media
+      $scope.socialMediaLink=$location.absUrl();
+      $scope.$on('$locationChangeSuccess', function(event) {
+            $scope.socialMediaLink=$location.absUrl();
+      });
       // TODO : add language
       var tokens = location.href.split('/');
       $scope.service = tokens[6].split('?')[0];
