8dcddeece1d466887005e87586e436e2fe3f719a
SpacebelGis <davinci@SpacebelGIS.local>
2017-10-18 12:55:55 +0200

    Add filter to display abstract of each MTD in searchpage

--- web-ui/src/main/resources/catalog/components/search/resultsview/ResultsviewDirective.js
+++ web-ui/src/main/resources/catalog/components/search/resultsview/ResultsviewDirective.js
@@ -245,4 +245,17 @@
       };
     }]);
 
+  module.filter('regexOneSentence', function() {
+     return function(val){
+       var RegExp = /^.*?[\.!\?](?:\s|$)/;
+       var match = RegExp.exec(val);
+       if (!match){
+        return val
+       }
+       else {
+        return match[0];
+       }
+     };
+  });
+
 })();
