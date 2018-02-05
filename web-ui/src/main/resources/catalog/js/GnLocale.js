180fb74143310c45d26a3a44451011f2809274c1
Francois Prunayre <fx.prunayre@gmail.com>
2018-02-05 10:37:44 +0100

    Remove one record / special character are not correct #2497.

--- web-ui/src/main/resources/catalog/js/GnLocale.js
+++ web-ui/src/main/resources/catalog/js/GnLocale.js
@@ -141,8 +141,8 @@
       );
 
       $translateProvider.preferredLanguage(gnGlobalSettings.iso3lang);
-      // $translateProvider.useSanitizeValueStrategy('escape');
-      $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
+      $translateProvider.useSanitizeValueStrategy('escapeParameters');
+      // $translateProvider.useSanitizeValueStrategy('sanitizeParameters');
 
       moment.locale(gnGlobalSettings.lang);
     }]);
