3ffe5bd3bfdb7088626f0c421f7e3aacd2b0d242
Francois Prunayre <fx.prunayre@gmail.com>
2019-07-01 16:59:58 +0200

    Minor db fix.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.6.2/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.6.2/migrate-default.sql
@@ -6,3 +6,15 @@ UPDATE metadata
       data,
       'http://geoservices.wallonie.be/', 'https://geoservices.wallonie.be/')
   WHERE data LIKE '%http://geoservices.wallonie.be/%';
+
+UPDATE metadata
+  SET data = REPLACE(
+      data,
+      'http://geoportail.wallonie.be', 'https://geoportail.wallonie.be')
+  WHERE data LIKE '%http://geoportail.wallonie.be%';
+
+UPDATE metadata
+  SET data = REPLACE(
+      data,
+      'http://geoportail.wallonie.be/ressources/id/', 'http://geodata.wallonie.be/id/')
+  WHERE data LIKE '%http://geoportail.wallonie.be/ressources/id/%';
