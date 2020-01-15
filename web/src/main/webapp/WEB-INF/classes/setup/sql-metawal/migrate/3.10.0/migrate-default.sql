24448a1858da943fa9dbd88910aff1094148ee62
Francois Prunayre <fx.prunayre@gmail.com>
2020-01-15 10:17:45 +0100

    SQL migration / Remove subtemplate. Update default language.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
@@ -2,5 +2,10 @@ DROP TABLE linkstatus;
 DROP TABLE metadatalink;
 DROP TABLE links;
 
+DELETE FROM metadata WHERE schemaid = 'iso19115-3';
+
+UPDATE languages SET isDefault = 'n' WHERE id = 'eng';
+UPDATE languages SET isDefault = 'y' WHERE id = 'fre';
+
 UPDATE Settings SET value='3.10.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
