08f65fb05f71a735b93d217d2fb9905da47f1f9d
Francois Prunayre <fx.prunayre@gmail.com>
2020-01-17 10:42:55 +0100

    Fix status table content.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
@@ -7,5 +7,10 @@ DELETE FROM metadata WHERE schemaid = 'iso19115-3';
 UPDATE languages SET isDefault = 'n' WHERE id = 'eng';
 UPDATE languages SET isDefault = 'y' WHERE id = 'fre';
 
+UPDATE statusvalues SET type = 'task' WHERE id = 100;
+UPDATE statusvalues SET type = 'event' WHERE id > 49 AND id <100;
+
+DELETE FROM metadatastatus WHERE statusid IN (SELECT id FROM statusvalues WHERE type = 'workflow')
+
 UPDATE Settings SET value='3.10.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
