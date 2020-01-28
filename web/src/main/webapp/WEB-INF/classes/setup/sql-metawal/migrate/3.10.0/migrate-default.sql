356cc8f8ddc98ba234f4fb6dad8b0bb00ec47f3b
Francois Prunayre <fx.prunayre@gmail.com>
2020-01-28 12:09:28 +0100

    CSW / Update configuration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.0/migrate-default.sql
@@ -12,5 +12,10 @@ UPDATE statusvalues SET type = 'event' WHERE id > 49 AND id <100;
 
 DELETE FROM metadatastatus WHERE statusid IN (SELECT id FROM statusvalues WHERE type = 'workflow')
 
+DELETE FROM cswservercapabilitiesinfo;
+DELETE FROM Settings WHERE name = 'system/csw/contactId';
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/csw/capabilityRecordUuid', '-1', 0, 1220, 'y');
+
+
 UPDATE Settings SET value='3.10.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
