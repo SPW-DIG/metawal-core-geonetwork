42c609795e92af3d09c69d8a80d7973f79ac8ded
Francois Prunayre <fx.prunayre@gmail.com>
2020-05-14 15:27:29 +0200

    Add more like this UI config.

--- web-ui/src/main/resources/catalog/js/CatController.js
+++ web-ui/src/main/resources/catalog/js/CatController.js
@@ -159,6 +159,14 @@ goog.require('gn_alert');
             //   _source: [field]
             // }
           },
+          'moreLikeThisConfig': {
+            "more_like_this" : {
+              "fields" : ["resourceTitleObject.default", "resourceAbstractObject.default", "tag.raw"],
+              "like" : null,
+              "min_term_freq" : 1,
+              "max_query_terms" : 12
+            }
+          },
           // TODOES
           'facetTabField': '',
           'facetConfig': {
