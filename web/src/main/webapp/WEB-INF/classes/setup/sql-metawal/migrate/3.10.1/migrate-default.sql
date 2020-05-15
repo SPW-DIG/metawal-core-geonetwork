a5cbea5e0e85aa534c0575c3b6bae8f4a5e04007
agf <arnaud.degroof@spacebel.be>
2020-05-15 08:14:02 +0200

    Add migration sql to 3.10.1

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/3.10.1/migrate-default.sql
@@ -0,0 +1,8 @@
+DELETE FROM cswservercapabilitiesinfo;
+DELETE FROM Settings WHERE name = 'system/csw/contactId';
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/csw/capabilityRecordUuid', '-1', 0, 1220, 'y');
+
+UPDATE metadata SET data = replace(data, '<mri:associatedResource/>', '') WHERE data LIKE '%<mri:associatedResource/>%';
+
+UPDATE Settings SET value='3.10.1' WHERE name='system/platform/version';
+UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
