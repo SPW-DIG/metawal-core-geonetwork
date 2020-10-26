85db9d2dbbff106ef90ca2d9d6bc7e7fb0d8ec4f
Francois Prunayre <fx.prunayre@gmail.com>
2020-10-26 09:52:52 +0100

    4.0.1 / SQL update

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.0.1/migrate-mw.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.0.1/migrate-mw.sql
@@ -4,5 +4,14 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 
 ALTER TABLE usersearch ALTER COLUMN url TYPE text;
 
+UPDATE settings SET internal = 'n' WHERE name = 'system/server/port';
+
+UPDATE metadata
+    SET data = replace(data,
+                       '>2019-07-02<',
+                       '>2019-05-22<')
+    WHERE data LIKE '%>2019-07-02<%';
+
+
 UPDATE Settings SET value='4.0.1' WHERE name='system/platform/version';
 UPDATE Settings SET value='SNAPSHOT' WHERE name='system/platform/subVersion';
