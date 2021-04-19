f66a760b34ca90d9772a1b406f4b38887b56f460
Francois Prunayre <fx.prunayre@gmail.com>
2021-04-19 12:27:28 +0200

    4.0.4 / SQL / Migration.

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.0.4/migrate-mw.sql
@@ -0,0 +1,15 @@
+
+DELETE FROM Schematrondes WHERE iddes IN (SELECT id FROM schematron WHERE filename LIKE 'schematron-rules-inspire%');
+DELETE FROM Schematroncriteria WHERE group_name || group_schematronid IN (SELECT name || schematronid FROM schematroncriteriagroup WHERE schematronid IN (SELECT id FROM schematron WHERE filename LIKE 'schematron-rules-inspire%'));
+DELETE FROM Schematroncriteriagroup WHERE schematronid IN (SELECT id FROM schematron WHERE filename LIKE 'schematron-rules-inspire%');
+DELETE FROM Schematron WHERE filename LIKE 'schematron-rules-inspire%';
+
+
+-- ALTER TABLE Settings ADD COLUMN encrypted VARCHAR(1) DEFAULT 'n';
+UPDATE Settings SET encrypted='y' WHERE name='system/proxy/password';
+UPDATE Settings SET encrypted='y' WHERE name='system/feedback/mailServer/password';
+UPDATE Settings SET encrypted='y' WHERE name='system/publication/doi/doipassword';
+
+
+UPDATE Settings SET value='4.0.4' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
