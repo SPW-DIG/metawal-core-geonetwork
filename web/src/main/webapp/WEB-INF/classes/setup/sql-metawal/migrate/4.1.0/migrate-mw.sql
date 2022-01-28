3ff221799971a7f8f58c616990575b05e0ff4c88
Francois Prunayre <fx.prunayre@gmail.com>
2022-01-28 08:59:33 +0100

    Metawal / Migration 4.1.0 and fix text color when colored background is used.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.0/migrate-mw.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.0/migrate-mw.sql
@@ -3,5 +3,9 @@ UPDATE Settings SET value='advanced', datatype=0, position= 2110, internal='n' W
 
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/publication/doi/doipublicurl', '', 0, 100196, 'n');
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadatacreate/preferredGroup', '', 1, 9105, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadatacreate/preferredTemplate', '', 0, 9106, 'n');
+
+
 UPDATE Settings SET value='4.1.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
