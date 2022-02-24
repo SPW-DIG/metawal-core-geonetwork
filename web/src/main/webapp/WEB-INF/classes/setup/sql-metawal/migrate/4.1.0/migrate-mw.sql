f025390859dd21a871986713659799ac9d68a73c
davinciagf <arnaud.degroof@spacebel.be>
2022-02-24 13:14:23 +0100

    Update DB (script related to the passwordEnforcement - from migration script 4.0.4 ?)

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.0/migrate-mw.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.0/migrate-mw.sql
@@ -6,6 +6,13 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadatacreate/preferredGroup', '', 1, 9105, 'n');
 INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadatacreate/preferredTemplate', '', 0, 9106, 'n');
 
+-- https://github.com/SPW-DIG/metawal-core-geonetwork/issues/619 -- from migration script version 4.0.4 !!!
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/minLength', '6', 1, 12000, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/maxLength', '20', 1, 12001, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/usePattern', 'true', 2, 12002, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/security/passwordEnforcement/pattern', '^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).*)$', 0, 12003, 'n');
+
+
 
 UPDATE Settings SET value='4.1.0' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
