8d76081f2ebc5fc58dfeae2f8c8a9a7f071e8c65
Francois Prunayre <fx.prunayre@gmail.com>
2022-05-16 11:17:22 +0200

    MW / SQL migration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.1/migrate-mw.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.1.1/migrate-mw.sql
@@ -3,6 +3,12 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system
 UPDATE Settings SET editable = 'n' WHERE name = 'system/userFeedback/lastNotificationDate';
 UPDATE Settings SET editable = 'n' WHERE name = 'system/security/passwordEnforcement/pattern';
 
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/csvReport/csvName', 'metadata_{datetime}.csv', 0, 12607, 'n');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metadata/link/excludedUrlPattern', '', 0, 12010, 'n');
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/metadata/thesaurusNamespace', 'https://registry.geonetwork-opensource.org/{{type}}/{{filename}}', 0, 9161, 'n');
+
 
 UPDATE Settings SET value='4.1.1' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
