64937e62c105ef0c49896b46583a6f32bdd97cc2
Francois Prunayre <fx.prunayre@gmail.com>
2025-04-16 08:23:18 +0200

    MW / 4.4.7 migration.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.4.7/migration-447.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.4.7/migration-447.sql
@@ -1,34 +1,151 @@
-UPDATE metadata  SET data = replace(data, 'https://geoportail.wallonie.be/walonmap/?#ADU', 'https://geoportail.wallonie.be/walonmap/#ADU')WHERE data LIKE  '%https://geoportail.wallonie.be/walonmap/?#ADU%';
-UPDATE metadata  SET data = replace(data, 'https://geoportail.wallonie.be/walonmap#ADU', 'https://geoportail.wallonie.be/walonmap/#ADU')WHERE data LIKE  '%https://geoportail.wallonie.be/walonmap#ADU%';
 
-WITH ns AS (select ARRAY [ARRAY ['xlink', 'http://www.w3.org/1999/xlink'],
-    ARRAY ['mdb', 'http://standards.iso.org/iso/19115/-3/mdb/2.0'],
-    ARRAY ['cit', 'http://standards.iso.org/iso/19115/-3/cit/2.0'],
-    ARRAY ['mri', 'http://standards.iso.org/iso/19115/-3/mri/1.0'],
-    ARRAY ['mco', 'http://standards.iso.org/iso/19115/-3/mco/1.0'],
-    ARRAY ['mrd', 'http://standards.iso.org/iso/19115/-3/mrd/1.0'],
-    ARRAY ['xsi', 'http://www.w3.org/2001/XMLSchema-instance'],
-    ARRAY ['gco', 'http://standards.iso.org/iso/19115/-3/gco/1.0']] AS n)
-UPDATE metadata
-SET data = REPLACE(data, aduLink, 'https://geoportail.wallonie.be/walonmap#PANIER=' || url_utf8_percent_encode(
-        '[{"serviceId":"1","visible":true,"url":"' || esriRest || '","label":' || to_json(currentTitle)::text ||
-        ',"type":"AGS_DYNAMIC","metadataUrl":"https://geodata.wallonie.be/doc/' || currentId || '"}]'::text))
-    FROM (SELECT distinct(
-                         unnest(xpath('//mdb:identificationInfo/*/mri:citation/*/cit:title/*/text()',
-                                      XMLPARSE(DOCUMENT data), n)))::text AS currentTitle,
-                     data                                                 as currentXml,
-                     uuid                                                 as currentId,
-                     unnest(xpath('//mrd:onLine/*[cit:protocol/*/text() = ''ESRI:REST'']/cit:linkage/*/text()',
-                                  XMLPARSE(DOCUMENT data), n))::text      AS esriRest,
-                     unnest(xpath(
-                             '//mrd:onLine/*/cit:linkage/*/text()[contains(., ''https://geoportail.wallonie.be/walonmap/#ADU'')]',
-                             XMLPARSE(DOCUMENT data), n))::text           AS aduLink,
-                     unnest(xpath('//mdb:metadataScope/*/mdb:resourceScope/*/@codeListValue',
-                                  XMLPARSE(DOCUMENT data), n))::text      AS resourceScope
-      FROM metadata,
-           ns
-      WHERE data LIKE '%https://geoportail.wallonie.be/walonmap/#ADU%'
-      ORDER BY uuid) AS records
-WHERE resourceScope != 'service'
-  AND metadata.uuid = records.currentId
-  AND esriRest != '';
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/provider', '', 0, 7301, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/serviceUrl', '', 0, 7302, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/translation/apiKey', '', 0, 7303, 'y');
+
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/languages', '', 0, 646, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/languages');
+INSERT INTO Settings (name, value, datatype, position, internal) SELECT distinct 'system/feedback/translationFollowsText', '', 0, 647, 'n' from settings WHERE NOT EXISTS (SELECT name FROM Settings WHERE name = 'system/feedback/translationFollowsText');
+
+UPDATE Settings SET value='4.4.5' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
+
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/userSelfRegistration/domainsAllowed', '', 0, 1911, 'y');
+UPDATE Settings SET value='4.4.6' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
+
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/banner/enable', 'false', 2, 1920, 'n');
+INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('system/auditable/enable', 'false', 2, 12010, 'n');
+
+ALTER TABLE Sources ADD COLUMN datahubEnabled BOOLEAN DEFAULT FALSE;
+ALTER TABLE Sources ADD COLUMN datahubConfiguration TEXT DEFAULT '';
+
+create table doiservers
+(
+  id                  integer      not null
+    primary key,
+  description         varchar(255),
+  landingpagetemplate varchar(255) not null,
+  name                varchar(32)  not null,
+  password            varchar(128),
+  pattern             varchar(255) not null,
+  prefix              varchar(15)  not null,
+  publicurl           varchar(255) not null,
+  url                 varchar(255) not null,
+  username            varchar(128)
+);
+
+create sequence doiserver_id_seq
+  start with 100;
+
+create table doiservers_group
+(
+  doiserver_id integer not null
+    constraint fk68kkdbdvxs6np07y6p3gfff5b
+      references doiservers,
+  group_id     integer not null
+    constraint fkk9nywysss6bm8hgccdqoqqyti
+      references groups,
+  primary key (doiserver_id, group_id)
+);
+
+ALTER TABLE groups ADD COLUMN minimumprofileforprivileges varchar(255);
+
+create table revinfo
+(
+  rev      integer not null
+    primary key,
+  revtstmp bigint
+);
+
+create table userauditable
+(
+  id                   integer not null
+    primary key,
+  createdby            varchar(255),
+  createddate          timestamp,
+  lastmodifiedby       varchar(255),
+  lastmodifieddate     timestamp,
+  address              varchar(255),
+  city                 varchar(255),
+  country              varchar(255),
+  emailaddress         varchar(255),
+  enabled              boolean not null,
+  groupseditor         varchar(255),
+  groupsregistereduser varchar(255),
+  groupsreviewer       varchar(255),
+  groupsuseradmin      varchar(255),
+  kind                 varchar(255),
+  name                 varchar(255),
+  organisation         varchar(255),
+  profile              varchar(255),
+  state                varchar(255),
+  surname              varchar(255),
+  username             varchar(255),
+  zip                  varchar(255)
+);
+
+create table userauditable_aud
+(
+  id                       integer not null,
+  rev                      integer not null
+    constraint fko4k6fftaj6wriots1r1tnihlp
+      references revinfo,
+  revtype                  smallint,
+  createdby                varchar(255),
+  createddate              timestamp,
+  lastmodifiedby           varchar(255),
+  lastmodifieddate         timestamp,
+  address                  varchar(255),
+  address_mod              boolean,
+  city                     varchar(255),
+  city_mod                 boolean,
+  country                  varchar(255),
+  country_mod              boolean,
+  emailaddress             varchar(255),
+  emailaddress_mod         boolean,
+  enabled                  boolean,
+  enabled_mod              boolean,
+  groupseditor             varchar(255),
+  groupseditor_mod         boolean,
+  groupsregistereduser     varchar(255),
+  groupsregistereduser_mod boolean,
+  groupsreviewer           varchar(255),
+  groupsreviewer_mod       boolean,
+  groupsuseradmin          varchar(255),
+  groupsuseradmin_mod      boolean,
+  kind                     varchar(255),
+  kind_mod                 boolean,
+  name                     varchar(255),
+  name_mod                 boolean,
+  organisation             varchar(255),
+  organisation_mod         boolean,
+  profile                  varchar(255),
+  profile_mod              boolean,
+  state                    varchar(255),
+  state_mod                boolean,
+  surname                  varchar(255),
+  surname_mod              boolean,
+  username                 varchar(255),
+  username_mod             boolean,
+  zip                      varchar(255),
+  zip_mod                  boolean,
+  primary key (id, rev)
+);
+
+create table spg_page_group
+(
+  language varchar(255) not null,
+  linktext varchar(255) not null,
+  groupid  integer      not null
+    constraint fkb1kxiuynecb84x003nwsrbifs
+      references groups,
+  primary key (language, linktext, groupid),
+  constraint fk9mlx8ch2q2f67w745pjx5ggvg
+    foreign key (language, linktext) references spg_page
+);
+
+
+UPDATE Settings SET value='4.4.7' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
+
