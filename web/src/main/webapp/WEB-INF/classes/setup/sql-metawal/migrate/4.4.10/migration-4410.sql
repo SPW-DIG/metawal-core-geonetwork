1ac23856c6a86d718cb2334aae00929d4905d78a
Francois Prunayre <fx.prunayre@gmail.com>
2026-06-04 16:17:22 +0200

    MW / Update to 4.4.11.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.4.10/migration-4410.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-metawal/migrate/4.4.10/migration-4410.sql
@@ -46,3 +46,14 @@ INSERT INTO Settings (name, value, datatype, position, internal) VALUES ('metada
 UPDATE Settings SET value='4.4.10' WHERE name='system/platform/version';
 UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
 
+UPDATE settings_ui
+SET configuration = replace(
+  configuration,
+  '"gn-recordview-manage-menu"',
+  '"gn-recordview-manage-menu","gn-recordview-download-menu"'
+                    )
+WHERE configuration LIKE '%"gn-recordview-manage-menu"%'
+  AND configuration NOT LIKE '%"gn-recordview-download-menu"%';
+
+UPDATE Settings SET value='4.4.11' WHERE name='system/platform/version';
+UPDATE Settings SET value='0' WHERE name='system/platform/subVersion';
