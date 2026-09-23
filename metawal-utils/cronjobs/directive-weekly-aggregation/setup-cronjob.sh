cd3348af1b52904b146b825aa844ae3702d1220b
GeryNi <gery.nicolay@spacebel.be>
2026-09-23 13:26:53 +0200

    updated cronjob directive-weekly-agregation (imrpoved query).

--- metawal-utils/cronjobs/directive-weekly-aggregation/setup-cronjob.sh
+++ metawal-utils/cronjobs/directive-weekly-aggregation/setup-cronjob.sh
@@ -1,2 +1,2 @@
-mkdir -p /var/log/weekly-aggregation/opendata/
-(crontab -l 2>/dev/null; echo "0 0 * * 1 /home/sites/metawal/sources/cronjobs/opendata-weekly-aggregation/opendata-weekly-aggregation.sh >> /var/log/weekly-aggregation/opendata/opendata-weekly-aggregation-\$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log 2>&1") | crontab -
+mkdir -p /var/log/weekly-aggregation/directive/
+(crontab -l 2>/dev/null; echo "0 0 * * 1 /home/sites/metawal/sources/cronjobs/directive-weekly-aggregation/directive-weekly-aggregation.sh >> /var/log/weekly-aggregation/directive/directive-weekly-aggregation-\$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log 2>&1") | crontab -
